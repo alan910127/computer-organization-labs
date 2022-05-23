51ca9bbba3ef6bfbae04db2bd918f1c3a8a4068c
alan910127 <alan469357@gmail.com>
2022-05-23 07:34:28 +0000

    Add I-type ALU control output

--- Lab5/Lab5Code/ALU_Ctrl.v
+++ Lab5/Lab5Code/ALU_Ctrl.v
@@ -14,15 +14,22 @@ reg ALU_Ctrl_o;
 
 always @(*) begin
     case (ALUOp)
-        2'b00: ALU_Ctrl_o = 4'b0010; //addi
+        2'b00: begin // I-type
+            case (func3)
+                3'b000: ALU_Ctrl_o = 4'b0010; // addi
+                3'b010: ALU_Ctrl_o = 4'b0111; // slti
+                3'b001: ALU_Ctrl_o = 4'b1000; // slli
+                default: ALU_Ctrl_o = 4'b0000;
+            endcase
+        end
         2'b01: ALU_Ctrl_o = 4'b0110; //branch
-        2'b10: begin //R-type
+        2'b10: begin // R-type
             case (instr)
-                4'b0000: ALU_Ctrl_o = 4'b0010; //add
-                4'b1000: ALU_Ctrl_o = 4'b0110; //sub
-                4'b0111: ALU_Ctrl_o = 4'b0000; //and
-                4'b0110: ALU_Ctrl_o = 4'b0001; //or
-                4'b0010: ALU_Ctrl_o = 4'b0111; //slt
+                4'b0000: ALU_Ctrl_o = 4'b0010; // add
+                4'b1000: ALU_Ctrl_o = 4'b0110; // sub
+                4'b0111: ALU_Ctrl_o = 4'b0000; // and
+                4'b0110: ALU_Ctrl_o = 4'b0001; // or
+                4'b0010: ALU_Ctrl_o = 4'b0111; // slt
                 default: ALU_Ctrl_o = 4'b0000;
             endcase
         end

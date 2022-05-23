26d32b27d8be98b011a0df0e7bb61605eeacaa1a
vinchantle <chenvincent610@gmail.com>
2022-05-23 15:28:08 +0800

    initial commit

--- Lab5/Lab5Code/MEMWB_register.v
+++ Lab5/Lab5Code/MEMWB_register.v
@@ -23,20 +23,6 @@ reg [4:0] WBreg_s;
 reg [31:0] pc_add4_s;
 
 always @(posedge clk_i) begin
-    WB_o <= WB_s;
-    DM_o <= DM_s;
-    alu_ans_o <= alu_ans_s;
-    WBreg_o <= WBreg_s;
-    pc_add4_o <= pc_add4_s;
-
-    WB_s <= WB_i;
-    DM_s <= DM_i;
-    alu_ans_s <= alu_ans_i;
-    WBreg_s <= WBreg_i;
-    pc_add4_s <= pc_add4_i;
-end
-
-always(posedge clk_i) begin
     if(~rst_i) begin
         WB_s <= 0;
         DM_s <= 0;
@@ -44,5 +30,19 @@ always(posedge clk_i) begin
         WBreg_s <= 0;
         pc_add4_s <= 0;
     end
+    else begin
+        WB_o <= WB_s;
+        DM_o <= DM_s;
+        alu_ans_o <= alu_ans_s;
+        WBreg_o <= WBreg_s;
+        pc_add4_o <= pc_add4_s;
+
+        WB_s <= WB_i;
+        DM_s <= DM_i;
+        alu_ans_s <= alu_ans_i;
+        WBreg_s <= WBreg_i;
+        pc_add4_s <= pc_add4_i;
+    end
 end
+
 endmodule

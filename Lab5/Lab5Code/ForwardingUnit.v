538c4ef7e358ef21729aa9c498b000ef83bb015c
alan910127 <alan469357@gmail.com>
2022-05-23 08:51:57 +0000

    Forwarding unit finished

--- Lab5/Lab5Code/ForwardingUnit.v
+++ Lab5/Lab5Code/ForwardingUnit.v
@@ -1,4 +1,8 @@
 `timescale 1ns/1ps
+`define SELECT_EXEMEM 2'h2
+`define SELECT_IDEXE  2'h1
+`define SELECT_ORIGINAL 2'h0
+
 module ForwardingUnit (
     input [5-1:0] IDEXE_RS1,
     input [5-1:0] IDEXE_RS2,
@@ -10,5 +14,17 @@ module ForwardingUnit (
     output reg [2-1:0] ForwardB
 );
 /* Write your code HERE */
+
+always @(*) begin
+    // EXEMEM has priority over MEMWB
+    if ((EXEMEM_RegWrite) && (EXEMEM_RD != 0) && (EXEMEM_RD == IDEXE_RS1)) ForwardA = `SELECT_EXEMEM;
+    else if ((MEMWB_RegWrite) && (MEMWB_RD != 0) && (MEMWB_RD == IDEXE_RS1)) ForwordA = `SELECT_IDEXE;
+    else ForwordA = `SELECT_ORIGINAL;
+
+    if ((EXEMEM_RegWrite) && (EXEMEM_RD != 0) && (EXEMEM_RD == IDEXE_RS2)) ForwardB = `SELECT_EXEMEM;
+    else if ((MEMWB_RegWrite) && (MEMWB_RD != 0) && (MEMWB_RD == IDEXE_RS2)) ForwordB = `SELECT_IDEXE;
+    else ForwordB = `SELECT_ORIGINAL;
+end
+
 endmodule
 
